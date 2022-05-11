7c7a9a19cb2294226f3e9ae9309dc73fed9e3e8c
DESKTOP-FS2R161\Alexander <AlexGan1kel@gmail.com>
2022-05-12 00:13:07 +0300

    add littleRobotEndStep event and listener

---
+++ src/model/events/LittleRobotEndStepEvent.java
@@ -0,0 +1,9 @@
+package model.events;
+
+import java.util.EventObject;
+
+public class LittleRobotEndStepEvent extends EventObject {
+    public LittleRobotEndStepEvent(Object source) {
+        super(source);
+    }
+}
---
+++ src/model/listeners/LittleRobotEndStepListener.java
@@ -0,0 +1,7 @@
+package model.listeners;
+
+import model.events.LittleRobotEndStepEvent;
+
+public interface LittleRobotEndStepListener {
+    public void littleRobotEndedStep(LittleRobotEndStepEvent e);
+}
