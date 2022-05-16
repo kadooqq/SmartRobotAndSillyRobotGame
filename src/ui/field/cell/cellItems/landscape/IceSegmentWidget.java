5eb16a199688a0667673a1480a3614bdd7ab12a8
DESKTOP-FS2R161\Alexander <AlexGan1kel@gmail.com>
2022-05-16 13:13:59 +0300

    remove unnecessary methods from landscape segment widgets

--- src/ui/field/cell/cellItems/landscape/IceSegmentWidget.java
+++ src/ui/field/cell/cellItems/landscape/IceSegmentWidget.java
@@ -2,23 +2,10 @@ package ui.field.cell.cellItems.landscape;
 
 import ui.utils.ImageUtils;
 
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.IOException;
 
 public class IceSegmentWidget extends LandscapeSegmentWidget {
     private static File imageFile = null;
-    @Override
-    protected BufferedImage getImage() {
-        BufferedImage image = null;
-        try {
-            image = ImageIO.read(getImageFile());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return image;
-    }
 
     @Override
     protected File getImageFile() {
